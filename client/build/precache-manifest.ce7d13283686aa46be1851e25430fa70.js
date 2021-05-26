278c2a4bc760c785b801725a7eacfa7404c145c2
Donghyun Kim <dean.kim0507@gmail.com>
2021-05-25 21:01:37 -0600

    commit

---
+++ client/build/precache-manifest.ce7d13283686aa46be1851e25430fa70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7214232f096ae9f0ebadb8a8695f17ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a74fc8ffbe10d2da7e86",
+    "url": "/static/css/2.a1251ad2.chunk.css"
+  },
+  {
+    "revision": "97d8517769825e95ef64",
+    "url": "/static/css/main.156920d3.chunk.css"
+  },
+  {
+    "revision": "a74fc8ffbe10d2da7e86",
+    "url": "/static/js/2.7712d25c.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.7712d25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d8517769825e95ef64",
+    "url": "/static/js/main.bdbc8b82.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
